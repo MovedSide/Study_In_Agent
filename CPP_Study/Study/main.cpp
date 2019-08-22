e52263405ae726246f057a87022c57a09d58ad0e
Moveside <jekker77@gmail.com>
2019-08-22 09:14:49 +0900

    cpp 임시 파일

--- CPP_Study/Study/main.cpp
+++ CPP_Study/Study/main.cpp
@@ -2,11 +2,17 @@
 #include<stdio.h>
 
 
+int& fn(int &x)
+{
+	return x;
+}
+
+
 int main()
 {
-	printf("Hello World!");
-
-
-
+	int a = 7;
+	int b = 2;
+	int &p = a;
+	printf("%d", fn(a));
 	return 0;
 }
