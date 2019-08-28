aaaa2ae38b63bd6e002ad6f2f42d4415ce1cb60b
Moveside <jekker77@gmail.com>
2019-08-28 11:52:04 +0900

    연산자 오버로딩 연습

--- CPP_Study/Study/main.cpp
+++ CPP_Study/Study/main.cpp
@@ -2,17 +2,52 @@
 #include<stdio.h>
 
 
-int& fn(int &x)
+class numb
 {
-	return x;
+private:
+	int n1, n2;
+public:
+	numb();
+	numb(int a, int b);
+	void show_numb();
+	
+	numb operator+(const numb& t);
+	numb operator+=(const numb& t);
+
+};
+numb::numb()
+{
+	this->n1 = 0;
+	this->n2 = 0;
 }
-
-
+numb::numb(int a, int b)
+{
+	this->n1 = a;
+	this->n2 = b;
+}
+numb numb::operator+(const numb& t)
+{
+	numb tmp(this->n1 + t.n1, this->n2 + t.n2);
+	return tmp;
+}
+numb numb::operator+=(const numb& t)
+{
+	numb tmp(0, 0);
+	*this = *this + t;
+	return tmp;
+}
+void numb::show_numb()
+{
+	std::cout << "n1 = " << this->n1 << std::endl;
+	std::cout << "n2 = " << this->n2 << std::endl;
+}
 int main()
 {
-	int a = 7;
-	int b = 2;
-	int &p = a;
-	printf("%d", fn(a));
+	numb a(1, 1);
+	numb b(2, 4);
+	numb c;
+	a += b;
+	a.show_numb();
+	b.show_numb();
 	return 0;
 }
